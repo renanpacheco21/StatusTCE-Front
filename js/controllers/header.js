bf36cd64fcf2ebc33dd89f8c6e6be21417fb1b6a
renanpacheco21 <85794910+renanpacheco21@users.noreply.github.com>
2021-11-20 09:04:55 -0300

    Delete js directory

--- js/controllers/header.js
+++
@@ -1,6 +0,0 @@
-angular.module('MyApp')
-    .controller('HeaderCtrl', function($scope, $location, $window, $auth, $rootScope) {
-        $scope.isActive = function(viewLocation) {
-            return viewLocation === $location.path();
-        };
-    });
